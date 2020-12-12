3c3535f5e64196796821dc70f8a0404f43197aa4
Gisan Geff <ggranraniego@gmail.com>
2020-12-12 15:08:21 +0800

    Added order transaction

--- admin/cashier.php
+++ admin/cashier.php
@@ -10,6 +10,73 @@ if (!isset($_SESSION['is_logged_in'])) {
 }
 
 
+if (isset($_POST['add-order'])) {
+
+    $user_id = -1;
+    $product_id = $_GET['id'];
+    $quantity = $_POST['quantity'];
+
+    $data = [
+        'cart_code' => 0,
+        'user_id' => $user_id,
+        'product_id' => $product_id
+    ];
+
+    $product = $function->searchInCart($data);
+    if (empty($product)) {
+
+        $data = [
+            'product_id' => $product_id,
+            'user_id' => $user_id,
+            'quantity' => $quantity,
+        ];
+
+        $query = "INSERT INTO cart (user_id, product_id, quantity) VALUES (:user_id, :product_id, :quantity)";
+        $function->insert($query, $data);
+
+    } else {
+
+        $quantity += $product['quantity'];
+        $data = [
+            'quantity' => $quantity,
+            'user_id' => $user_id,
+            'product_id' => $product_id
+        ];
+
+        $query = "UPDATE cart SET quantity = :quantity WHERE cart_code = 0 AND user_id = :user_id AND product_id = :product_id";
+        $function->update($query, $data);
+
+    }
+
+    //Update Product QuantityInStock
+    $data = ['quantity' => $_POST['quantity'], 'id' => $product_id];
+    $query = "UPDATE products SET QuantityInStock = (QuantityInStock - :quantity) WHERE id = :id";
+    $function->update($query, $data);
+
+
+  
+  }
+
+  //delete products
+if (isset($_GET['delete_id'])) {
+
+    $cart_id = $_GET['delete_id'];
+
+    $cart = $function->getData('cart', 'cart_id', $cart_id);
+
+    //Update Product QuantityInStock
+    $data = ['quantity' => $cart['quantity'], 'id' => $cart['product_id']];
+    $query = "UPDATE products SET QuantityInStock = (QuantityInStock + :quantity) WHERE id = :id";
+    $function->update($query, $data);
+
+    $data = ['cart_id' => $cart_id];
+    $query = "DELETE FROM cart WHERE cart_id = :cart_id";
+    $function->delete($query, $data);
+  
+
+  }
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,7 +84,7 @@ if (!isset($_SESSION['is_logged_in'])) {
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>Home | cashier</title>
+    <title>Cashier | Admin</title>
     <!-- Favicon-->
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
@@ -36,6 +103,9 @@ if (!isset($_SESSION['is_logged_in'])) {
 
     <!-- JQuery DataTable Css -->
     <link href="plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- Bootstrap Select Css -->
+    <link href="plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
 
     <!-- Custom Css -->
     <link href="css/style.css" rel="stylesheet">
@@ -107,9 +177,10 @@ if (!isset($_SESSION['is_logged_in'])) {
                                                                 <td><?php echo $row['price']; ?></td>
                                                                 <td><?php echo $row['QuantityInStock']; ?></td>
                                                                 <td class="text-center">
-                                                                    <a href="#" class="btn btn-success btn-xs waves-effect">
+                                                                    <a data-toggle="modal" data-target="#addModal_<?php echo $row['id']; ?>" class="btn btn-success btn-xs waves-effect">
                                                                         <i class="material-icons" style="font-size:1.6rem;">add</i>
                                                                     </a>
+                                                                    <?php include 'cashier-modal.php'; ?>
                                                                 </td>
                                                             </tr>
                                                         <?php
@@ -141,35 +212,47 @@ if (!isset($_SESSION['is_logged_in'])) {
                                                                 <th>Price</th>
                                                                 <th>Quantity</th>
                                                                 <th>Total</th>
-                                                                <th></th>
+                                                                <th class="text-center">Action</th>
                                                             </tr>
                                                         </thead>
                                                         <tbody>
+
+                                                        <?php
+                                                        
+                                                        $cart_total = 0;
+                                                        $query = "SELECT cart.cart_id, products.id, products.name, products.price, cart.quantity, (products.price * cart.quantity) AS 'total' FROM cart INNER JOIN products ON cart.product_id = products.id WHERE user_id = -1 AND cart_code = 0 GROUP BY cart.product_id ORDER BY cart_id";
+
+                                                        $rows = $function->selectAll($query);
+                                                        foreach ($rows as $row) { ?>
                                                             <tr>
-                                                              <td>
-                                                          
-                                                                <?php echo 'name'; ?></td>
-                                                              <td><?php echo 'price'; ?></td>
-                                                              <td class="text-center">
-                                                                <div class="btn-group">
-                                                                  <a href="#" class="btn btn-info btn-xs waves-effect">
-                                                                    <i class="material-icons" style="font-size:1.6rem;">remove</i>
-                                                                  </a>
-                                                                  <a class="btn btn-default btn-xs waves-effect">
-                                                                    <i class="material-icons" style="font-size:1.6rem;">12</i>
-                                                                  </a>
-                                                                  <a href="#" class="btn btn-info btn-xs waves-effect">
-                                                                    <i class="material-icons" style="font-size:1.6rem;">add</i>
-                                                                  </a>
-                                                                </div>
-                                                              </td>
-                                                              <td><?php echo 'Total'; ?></td>
-                                                              <td>
-                                                                <a href="#" class="btn btn-danger btn-xs waves-effect">
-                                                                  <i class="material-icons" style="font-size:1.6rem;">clear</i>
-                                                                </a>  
-                                                              </td>
+                                                                <td><?php echo $row['name']; ?></td>
+                                                                <td><?php echo $row['price']; ?></td>
+                                                                <td class="text-center">
+                                                                    <div class="btn-group">
+                                                                    <a href="#" class="btn btn-info btn-xs waves-effect">
+                                                                        <i class="material-icons" style="font-size:1.6rem;">remove</i>
+                                                                    </a>
+                                                                    <a class="btn btn-default btn-xs m-r-5">
+                                                                    <i class="material-icons" style="font-size:1.6rem;"></i>
+                                                                        <?php echo $row['quantity']; ?>
+                                                                    </a>
+                                                                    <a href="#" class="btn btn-info btn-xs waves-effect">
+                                                                        <i class="material-icons" style="font-size:1.6rem;">add</i>
+                                                                    </a>
+                                                                    </div>
+                                                                </td>
+                                                                <td><?php echo $row['total']; ?></td>
+                                                                <td  class="text-center">
+                                                                    <a href="?delete_id=<?php echo $row['cart_id']; ?>"" class="btn btn-danger btn-xs waves-effect">
+                                                                        <i class="material-icons" style="font-size:1.6rem;">clear</i>
+                                                                    </a>  
+                                                                </td>
                                                             </tr>
+
+                                                        <?php
+                                                            $cart_total += $row['total'];
+                                                        } ?>
+                                                            
                                                         </tbody>
                                                     </table>
                                                   </div>
@@ -178,10 +261,11 @@ if (!isset($_SESSION['is_logged_in'])) {
                                                   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-t-30">
                                                     <p class="pull-right">
                                                         <b>Cart Total</b>
-                                                        PHP 123,123.12
+                                                        PHP <?php echo number_format($cart_total, 2);  ?>
                                                     </p>
                                                   </div>
-                                                  <input type="submit" class="btn btn-primary pull-right" value="Checkout">
+                                                  <!-- <input type="submit" class="btn btn-primary pull-right" value="Checkout"> -->
+                                                  <a href="checkout.php" class="btn btn-primary pull-right">Checkout</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -210,6 +294,9 @@ if (!isset($_SESSION['is_logged_in'])) {
 
     <!-- Bootstrap Notify Plugin Js -->
     <script src="plugins/bootstrap-notify/bootstrap-notify.js"></script>
+
+    <!-- Jquery Spinner Plugin Js -->
+    <script src="plugins/jquery-spinner/js/jquery.spinner.js"></script>
 
     <!-- Waves Effect Plugin Js -->
     <script src="plugins/node-waves/waves.js"></script>
