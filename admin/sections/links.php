42abf99ca9d2f0380cf08e4377c2e1e8176beb64
Gisan Geff <ggranraniego@gmail.com>
2020-12-16 02:24:34 +0800

    Change icon

--- admin/sections/links.php
+++ admin/sections/links.php
@@ -1,5 +1,5 @@
     <!-- Favicon-->
-    <link rel="icon" href="../favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
